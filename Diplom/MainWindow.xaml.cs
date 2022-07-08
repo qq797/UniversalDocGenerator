ca8eb76f38a554360e92e8237527c40cdf182512
qq797 <75188569+qq797@users.noreply.github.com>
2022-07-08 14:07:57 +0300

    Добавьте файлы проекта.

---
+++ Diplom/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace UniversalDocGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для FSBWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Window_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+  // path of Document              var helper = new WordHelper("C:/Users/user/source/repos/UniversalDocumentGenerator/Diplom/WordPattern/PatternDoc.doc");
+
+                var items = new Dictionary<string, string>
+                {
+                    {
+                        "<REQUEST_NUMBER>", TS_NumberRequest_TextBox.Text
+                    },
+
+                    {
+                        "<TS_NAME>", TS_Name_TextBox.Text
+                    },
+
+                    {
+                        "<TS_MANUFACTURER>", TS_Manufacturer_TextBox.Text
+                    },
+
+                    {
+                        "<TS_MODEL>", TS_Model_TextBox.Text
+                    },
+
+                    {
+                        "<TS_SERIALNUMBER>", TS_SerialNumber_TextBox.Text
+                    },
+
+                    
+
+                };
+
+                helper.Process(items);
+            }
+
+            catch
+            {
+                Console.WriteLine("Файл не найден");
+            }
+        }
+    }
+}
---
+++ Diplom/WordHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace UniversalDocGenerator
+{
+    class WordHelper
+    {
+        private FileInfo fileInfo;
+       
+        public WordHelper(string filename)
+        {
+            if(File.Exists(filename))
+            {
+                fileInfo = new FileInfo(filename);
+            }
+            else
+            {
+                throw new Exception("Файл не найден");
+            }
+        }
+
+        internal void Process(Dictionary<string, string> items)
+        {
+            try
+            {
+                var app = new Word.Application();
+                Object file = fileInfo.FullName;
+                Object missing = Type.Missing;
+
+                app.Documents.Open(file);
+
+                foreach(var item in items)
+                {
+                    Word.Find find = app.Selection.Find;
+                    find.Text = item.Key;
+                    find.Replacement.Text = item.Value;
+
+                    Object wrap = Word.WdFindWrap.wdFindContinue;
+                    Object replace = Word.WdReplace.wdReplaceAll;
+
+                    find.Execute(FindText: Type.Missing,
+                        MatchCase: false,
+                        MatchWholeWord: false,
+                        MatchWildcards: false,
+                        MatchSoundsLike: missing,
+                        MatchAllWordForms: false,
+                        Forward: true,
+                        Wrap: wrap,
+                        Format: false,
+                        ReplaceWith: missing, Replace: replace);
+                }
+
+               
+                    string path = @"\\Diplom\SaveDoc\";
+                    string time = DateTime.Now.ToString("yyyymmDD");
+                    string newFileName = Path.Combine(path, time);
+                    app.ActiveDocument.SaveAs2(newFileName);
+                    app.ActiveDocument.Close();
+                
+
+                
+                MessageBox.Show("Документы сохранены");
+
+                //return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //return false;
+
+
+
+        }
+    }
+}
